670c8522aa3b5a3c037ad6eb03f97fc81432d3e4
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-07-21 22:12:04 +0000

    added in some tests that use what hte github action sets up

---
+++ tests/taqueria.spec.ts
@@ -0,0 +1,40 @@
+import { exec as exec1 } from 'child_process';
+import fsPromises from 'fs/promises';
+import util from 'util';
+const exec = util.promisify(exec1);
+
+describe('E2E Testing for taqueria action', () => {
+	test('Verify that taqueria flextesa plugin can return list of accounts from the local sandbox', async () => {
+		const accounts = await exec(`taq list accounts local`, { cwd: `./` });
+		expect(accounts.stdout).toContain('bob');
+        expect(accounts.stdout).toContain('alice');
+        expect(accounts.stdout).toContain('john');
+        expect(accounts.stdout).toContain('jane');
+        expect(accounts.stdout).toContain('joe');
+	});
+
+    test('Verify that taqueria can compile a previously registered contract', async () => {
+		const accounts = await exec(`taq compile`, { cwd: `./` });
+		expect(accounts.stdout).toContain('artifacts/hell-tacos.tz');
+	});
+
+    test('Verify that taqueria can originate a contract to the local sandbox', async () => {
+        const contractName = 'hello-tacos.tz'
+
+		const contractOriginate = await exec(`taq originate ${contractName}`, { cwd: `./` });
+        expect(contractOriginate.stdout).toContain(contractName);
+		expect(contractOriginate.stdout).toContain('local');
+
+        const contractHash = contractOriginate.stdout.split('\n')
+        .find(line => line.includes(contractName))
+        ?.split('│')[2]
+        .trim()
+
+        const sandboxContractContents = await exec(`http://localhost:20000/chains/main/blocks/head/context/contracts/${contractHash}`)
+        expect(sandboxContractContents.stdout).toContain('"storage":{"int":"42"}')
+
+	});
+});
+
+
+
