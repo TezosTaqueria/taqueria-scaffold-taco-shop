ea4dcda4307c2b46c205f4cdd09f38bcbce94e19
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-07-21 23:27:05 +0000

    more logging

--- tests/taqueria.spec.ts
+++ tests/taqueria.spec.ts
@@ -4,8 +4,8 @@ const execPromise = util.promisify(exec);
 
 describe('E2E Testing for taqueria action', () => {
 	test('Verify that taqueria flextesa plugin can return list of accounts from the local sandbox', async () => {
-        console.log(execPromise(`./taq`).stdout);
-        console.log(execPromise(`ls -al`).stdout);
+        console.log(await execPromise(`./taq`));
+        console.log(await execPromise(`ls -al`));
 		const accounts = execPromise(`taq list accounts local`);
         console.log(accounts)
 		// expect(accounts.stdout).toContain('bob');
