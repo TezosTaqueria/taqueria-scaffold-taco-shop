e48110cba5db9f1c6b5b081cf368e81d69e67439
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-07-22 15:18:48 +0000

    re-enabling tests now that taq is accessible

--- tests/taqueria.spec.ts
+++ tests/taqueria.spec.ts
@@ -9,29 +9,34 @@ describe('E2E Testing for taqueria action', () => {
 		const accounts = await execPromise(`taq list accounts local`);
         console.log(accounts)
         console.log(accounts.stdout)
+		expect(accounts.stdout).toContain('bob');
+        expect(accounts.stdout).toContain('alice');
+        expect(accounts.stdout).toContain('john');
+        expect(accounts.stdout).toContain('jane');
+        expect(accounts.stdout).toContain('joe');
 	});
 
-    // test('Verify that taqueria can compile a previously registered contract', async () => {
-	// 	const accounts = await execPromise(`taq compile`, { cwd: `./` });
-	// 	expect(accounts.stdout).toContain('artifacts/hell-tacos.tz');
-	// });
+    test('Verify that taqueria can compile a previously registered contract', async () => {
+		const accounts = await execPromise(`taq compile`, { cwd: `./` });
+		expect(accounts.stdout).toContain('artifacts/hell-tacos.tz');
+	});
 
-    // test('Verify that taqueria can originate a contract to the local sandbox', async () => {
-    //     const contractName = 'hello-tacos.tz'
+    test('Verify that taqueria can originate a contract to the local sandbox', async () => {
+        const contractName = 'hello-tacos.tz'
 
-	// 	const contractOriginate = await execPromise(`taq originate ${contractName}`, { cwd: `./` });
-    //     expect(contractOriginate.stdout).toContain(contractName);
-	// 	expect(contractOriginate.stdout).toContain('local');
+		const contractOriginate = await execPromise(`taq originate ${contractName}`, { cwd: `./` });
+        expect(contractOriginate.stdout).toContain(contractName);
+		expect(contractOriginate.stdout).toContain('local');
 
-    //     const contractHash = contractOriginate.stdout.split('\n')
-    //     .find(line => line.includes(contractName))
-    //     ?.split('│')[2]
-    //     .trim()
+        const contractHash = contractOriginate.stdout.split('\n')
+        .find(line => line.includes(contractName))
+        ?.split('│')[2]
+        .trim()
 
-    //     const sandboxContractContents = await execPromise(`http://localhost:20000/chains/main/blocks/head/context/contracts/${contractHash}`)
-    //     expect(sandboxContractContents.stdout).toContain('"storage":{"int":"42"}')
+        const sandboxContractContents = await execPromise(`http://localhost:20000/chains/main/blocks/head/context/contracts/${contractHash}`)
+        expect(sandboxContractContents.stdout).toContain('"storage":{"int":"42"}')
 
-	// });
+	});
 });
 
 
