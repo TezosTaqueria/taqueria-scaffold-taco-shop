a79560a343ebf1a502d37d96a7438fec26f291e7
Edward Garson <egarson@gmail.com>
2023-01-12 15:12:32 -0800

    Add more basic UI tests and a couple of TODOs

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,8 +1,23 @@
 import { screen } from '@testing-library/react';
 
 describe('Taco Shop page', () => {
-  test('displays the correct title', () => {
-    const titleValue = screen.queryAllByText('Taco Shop');
-    expect(titleValue).toBeDefined();
-  });
+
+    it('should display the correct title', () => {
+        const element = screen.queryAllByText('Taco Shop');
+        expect(element).toBeDefined();
+    });
+
+    it('should initially display "0" in the button text', () => {
+        const element = screen.queryAllByText('Order 0 Tacos');
+        expect(element).toBeDefined();
+    });
+
+    xit('should update the button text when the spinner is incremented', () => {
+        // TODO
+    });
+
+    xit('should decrement the taco count when clicked', () => {
+        // TODO
+    });
+
 });
