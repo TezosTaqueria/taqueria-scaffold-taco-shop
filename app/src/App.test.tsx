32ffb2e0b8f6b25d982bc6ef0a23f618fe9a3829
Edward Garson <egarson@gmail.com>
2023-01-19 18:16:31 -0800

    Add header blurb, todo test in Front-End tests

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,3 +1,16 @@
+/*
+  The tests in this module exercise the Front-End, user-facing tests. They are concerned with
+  widgets, i.e. that visual controls align with the state of the deployed contract.
+
+  The Front End uses React, so this test code is somewhat coupled to that framework.
+  Note that there are also Integration Tests and Unit Tests for the Contract itself.
+
+  Please see the top-level README file for more information.
+
+  Copyright (c) 2023 ECAD Labs. All rights reserved.
+  This software is licensed under the terms of the included LICENSE file.
+*/
+
 import { screen } from '@testing-library/react';
 
 describe('Taco Shop page', () => {
@@ -17,6 +30,10 @@ describe('Taco Shop page', () => {
     });
 
     xit('should decrement the taco count when clicked', () => {
+        // TODO
+    });
+
+    xit('should disable the purchase button if amount exceeds available', () => {
         // TODO
     });
 
